0e97d9d319ada9cdf0f0fd7f9657bb88fd0112a3
NickMcD4 <nickmcd4@gmail.com>
2018-07-30 00:24:47 -0600

    omdb app done

--- liri.js
+++ liri.js
@@ -12,11 +12,10 @@ var client = new Twitter(keys.twitter);
 
 var nodeArgs = process.argv;
 var command = process.argv[2];
-
 // Create an empty string for holding the full query
 var query = "";
 
-// Capture all the words in the query (again ignoring the first two Node arguments)
+// Capture all the words in the query (again ignoring the first three Node arguments)
 for (var i = 3; i < nodeArgs.length; i++) {
 
   // Build a string with the query.
@@ -24,65 +23,123 @@ for (var i = 3; i < nodeArgs.length; i++) {
 }
 
 
+var movieTitle = ""
+var songTitle = ""
+
+//if the third argument is "movie-this", run the imdb function
+if (command === "movie-this") {
+  omdbNow();
+}
+
+function omdbNow() {
+  //query becomes movie title
+  movieTitle = query;
+  //console.log(movieTitle);
+
+
+
+  //If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+  if (!movieTitle) {
+    movieTitle = "Mr Nobody";
+    console.log("If you haven't watched Mr. Nobody, then you should: http://www.imdb.com/title/tt0485947/");
+    console.log("It's on Netflix!");
+    // console.log("LOOK HERE" + movieTitle);
+  }
+
+  //request OMDB for movie info 
+  request("http://www.omdbapi.com/?t=" + movieTitle + "&apikey=trilogy", function (error, response, body) {
+    //signifies successful request
+    if (!error && response.statusCode === 200) {
+      var movieData = JSON.parse(body);
+      // console.log(movieData);
+      console.log("Title:" + movieData.Title + "\n");
+      console.log("Release Year:" + movieData.Year + "\n");
+      console.log("IMDB Rating:" + movieData.imdbRating + "\n");
+      console.log("Rotten Tomatoes Rating:" + movieData.Ratings[1].Value + "\n");
+      console.log("Produced in:" + movieData.Country + "\n");
+      console.log("Language:" + movieData.Language + "\n");
+      console.log("Plot:" + movieData.Plot + "\n");
+      console.log("Actors:" + movieData.Actors + "\n");
+
+
+    }
+
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*IMBD-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*//
 
-// var movieThis = function(movieQuery) {
-// 	// Load request npm module
-// 	var request = require("request");
 
-// 	// if query that is passed in is undefined, Mr. Nobody becomes the default
-// 	if(movieQuery === undefined) {
-// 		movieQuery = "mr nobody";
-// 	}
+// if(command === "movie-this" && nodeArgs !== undefined){
+// // Then run a request to the OMDB API with the movie specified
+// var queryUrl = "http://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=trilogy";
 
-// 	// HTTP GET request
-// 	request("http://www.omdbapi.com/?t=" + movieQuery + "&y=&plot=short&r=json", function(error, response, body) {
-// 	  if (!error && response.statusCode === 200) {
-// 	    console.log("* Title of the movie:         " + JSON.parse(body).Title);
-// 	    console.log("* Year the movie came out:    " + JSON.parse(body).Year);
-// 	    console.log("* IMDB Rating of the movie:   " + JSON.parse(body).imdbRating);
-// 	    console.log("* Country produced:           " + JSON.parse(body).Country);
-// 	    console.log("* Language of the movie:      " + JSON.parse(body).Language);
-// 	    console.log("* Plot of the movie:          " + JSON.parse(body).Plot);
-// 	    console.log("* Actors in the movie:        " + JSON.parse(body).Actors);
 
-// 	    // For loop parses through Ratings object to see if there is a RT rating
-// 	    // 	--> and if there is, it will print it
-// 	    for(var i = 0; i < JSON.parse(body).Ratings.length; i++) {
-// 	    	if(JSON.parse(body).Ratings[i].Source === "Rotten Tomatoes") {
-// 	    		console.log("* Rotten Tomatoes Rating:     " + JSON.parse(body).Ratings[i].Value);
-// 	    		if(JSON.parse(body).Ratings[i].Website !== undefined) {
-// 	    			console.log("* Rotten Tomatoes URL:        " + JSON.parse(body).Ratings[i].Website);
-// 	    		}
-// 	    	}
-// 	    }
-// 	  }
-// 	});
+// request(queryUrl, function(error, response, body) {
+
+//   // If the request is successful
+//   if (!error && response.statusCode === 200) {
+
+//     // Parse the body of the site and recover just the imdbRating
+//     // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+//     console.log("Title of the movie:         " + JSON.parse(body).Title);
+//     console.log("Release Year: " + JSON.parse(body).Year);
+//     console.log("IMDB Rating of the movie:   " + JSON.parse(body).imdbRating);
+//     console.log("Rotten Tomato Rating of the movie:   " + JSON.parse(body).Ratings[2].Value);
+//     console.log("Produced in:   " + JSON.parse(body).Country);
+//     console.log("Language:   " + JSON.parse(body).Language);
+//     console.log("Plot:   " + JSON.parse(body).Plot);
+//     console.log("Actors:   " + JSON.parse(body).Actors);
+//   }
+// });
+
 // }
+// if(command === "movie-this" && nodeArgs===""){
+// // Then run a request to the OMDB API with the movie specified
+// var queryUrl = "http://www.omdbapi.com/?t=" + "mr_nobody" + "&y=&plot=short&apikey=trilogy";
 
 
-// if(command === "movie-this") {
-//     movieThis(query);
+// request(queryUrl, function(error, response, body) {
+
+//   // If the request is successful
+//   if (!error && response.statusCode === 200) {
+
+//     // Parse the body of the site and recover just the imdbRating
+//     // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+
+//     console.log("If you haven't watched Mr. Nobody, then you should: http://www.imdb.com/title/tt0485947/");
+
+//   }
+// });
+
 // }
-
-if(command === "movie-this"){
-// Then run a request to the OMDB API with the movie specified
-var queryUrl = "http://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=trilogy";
-
-
-request(queryUrl, function(error, response, body) {
-
-  // If the request is successful
-  if (!error && response.statusCode === 200) {
-
-    // Parse the body of the site and recover just the imdbRating
-    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-    console.log("* Title of the movie:         " + JSON.parse(body).Title);
-    console.log("Release Year: " + JSON.parse(body).Year);
-    console.log("* IMDB Rating of the movie:   " + JSON.parse(body).imdbRating);
-    console.log(response);
-  }
-});
-
-}
